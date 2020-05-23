24ef4ea3f4fcc4361bce61d91ca3988e8a787247
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-05-23 14:26:22 +0800

    Update sigminer-diagram.R

--- paper/sigminer-diagram.R
+++ paper/sigminer-diagram.R
@@ -5,148 +5,120 @@ grViz("digraph course {
 rankdir = LR
 node [shape = box, style=filled]
 layout = dot
-compound =true
+compound =true 
 #color = crimson
 subgraph clusterA{
-label = 'ChIPseeker'
+label = 'Data Import And Transformation'
 style = dashed
 rank = same
-roi [label = 'Region of interest']
-gene2 [label = 'Gene in proximity']
+maf [label = 'Mutation Annotation File']
+readMaf [label = 'read_maf']
+tallyMaf [label = 'sig_tally']
 }
 subgraph clusterB{
 rankdir = LR
-label = 'Over Representation Analysis'
+label = 'De novo Signatures Discovery'
 style = dashed
 rank = same
 subgraph cluster0 {
-label = 'clusterProfiler'
-enrichGO
-enrichKEGG 
-enrichMKEGG
-enricher [label = 'enricher\nfor MSigDb, wikiPathway\nand many others']
+label = 'Manual extraction'
+sig_estimate
+show_sig_number_survey
+sig_extract
+sig1 [label = 'Signature object']
 }
 subgraph cluster1 {
-label = 'DOSE'
-enrichDO
-enrichNCG
-enrichDGN
-}
-subgraph cluster2 {
-label = 'ReactomePA'
-enrichPathway
-}
-subgraph cluster3 {
-label = 'meshes'
-enrichMeSH
+label = 'Auto extraction'
+sig_auto_extract
+sig2 [label = 'Signature object']
 }
 }
 subgraph clusterC{
-label = 'Gene Set Enrichment Analysis'
+label = 'Reference Signatures Fitting'
 style = dashed
 rank = same
 subgraph cluster0 {
-label = 'clusterProfiler'
-gseGO
-gseKEGG 
-gseMKEGG
-GSEA [label = 'GSEA\nfor MSigDb, wikiPathway\nand many others']
+label = 'Reference signatures'
+cosmic [label = 'COSMIC database']
+custsig [label = 'Custom signatures']
 }
 subgraph cluster1 {
-label = 'DOSE'
-gseDO
-gseNCG
-gseDGN
+label = 'Optimal fitting'
+sig_fit
 }
 subgraph cluster2 {
-label = 'ReactomePA'
-gsePathway
+label = 'Stability analysis\nfor single sample'
+sig_fit_bootstrap
+report_bootstrap_p_value
 }
 subgraph cluster3 {
-label = 'meshes'
-gseMeSH
+label = 'Stability analysis\nfor multiple samples'
+sig_fit_bootstrap_batch
 }
 }
 subgraph clusterD{
-label = 'Visualization using enrichplot'
+label = 'Visualization with ggplot2'
 style = dashed
 rank = same
 subgraph cluster1 {
-label = 'ORA only'
-barplot
-goplot
+label = 'Signature fitting'
+show_sig_fit
+show_sig_bootstrap_exposure
+show_sig_bootstrap_error
+show_sig_bootstrap_stability
 }
 subgraph cluster2 {
-label = 'ORA & GSEA'
-dotplot
-upsetplot
-heatplot
-cnetplot
-emapplot
-}
-subgraph cluster3 {
-label = 'GSEA only'
-gseaplot
-gseaplot2
-gsearank
-ridgeplot
+label = 'Signature/catalogue profile'
+show_sig_profile
+show_sig_exposure
+show_cosmic_sig_profile
+show_catalogue
 }
 }
-subgraph clusterE{
-label = 'Remove redundant terms'
-style = dashed
-rank = same
-simplify [label = 'simplify for GO only\nby GOSemSim']
-}
-subgraph clusterF{
-label = 'Compare different conditions'
-style = dashed
-rank = same
-comparecluster [label = 'compareCluster\nby clusterProfiler']
-}
-rnaseq [label = 'RNA-seq']
-gene [label = 'Gene of interest\ne.g. from pull-down']
-genelist [label='Ranked gene list']
-de [label = 'Differential expressed gene']
-roi->gene2
-#ora [label = 'Over Representation Analysis']
-#gsea [label = 'Gene Set Enrichment Analysis']
-microarray -> de
-rnaseq -> de
-microarray -> genelist #gseKEGG[lhead=clusterC]
-rnaseq -> genelist
-de -> enrichKEGG[lhead=clusterB]
-gene -> enrichKEGG[lhead=clusterB]
-gene2 -> enrichKEGG[lhead=clusterB]
-enrichKEGG -> enrichNCG [style = invis]
-enrichPathway -> enrichMeSH  [style = invis]
-gseKEGG -> gseNCG [style = invis]
-gsePathway -> gseMeSH  [style = invis]
-genelist -> gseMKEGG [lhead=clusterC]
-others [label = 'Other sources']
-others -> genelist
-enrichDGN -> barplot [ltail=clusterB]
-enrichDGN -> dotplot [ltail=clusterB]
-enrichDGN -> upsetplot [ltail=clusterB]
-enrichDGN -> heatplot [ltail=clusterB]
-enrichDGN -> cnetplot [ltail=clusterB]
-enrichDGN -> emapplot [ltail=clusterB]
-enrichGO -> goplot
-gseDGN -> ridgeplot [ltail=clusterC]
-gseDGN -> dotplot [ltail=clusterC]
-gseDGN -> upsetplot [ltail=clusterC]
-gseDGN -> heatplot [ltail=clusterC]
-gseDGN -> cnetplot [ltail=clusterC]
-gseDGN -> emapplot [ltail=clusterC]
-gseDGN -> gseaplot [ltail=clusterC]
-gseDGN -> gseaplot2 [ltail=clusterC]
-gseDGN -> gsearank [ltail=clusterC]
-enrichGO -> simplify
-gseGO -> simplify
-enrichMeSH -> comparecluster[ltail=clusterB]
+
+maf -> readMaf
+readMaf -> tallyMaf
+
+tallyMaf -> sig_estimate
+tallyMaf -> sig_extract
+tallyMaf -> sig_auto_extract
+tallyMaf -> sig_fit
+tallyMaf -> sig_fit_bootstrap
+tallyMaf -> sig_fit_bootstrap_batch
+tallyMaf -> show_catalogue
+
+sig_estimate -> show_sig_number_survey
+show_sig_number_survey -> sig_extract
+sig_extract -> sig1
+sig_auto_extract -> sig2
+
+sig1 -> custsig
+sig1 -> cosmic
+sig2 -> custsig
+sig2 -> cosmic
+
+sig_fit_bootstrap -> report_bootstrap_p_value
+
+sig_fit -> show_sig_fit
+sig_fit_bootstrap_batch -> show_sig_bootstrap_exposure
+sig_fit_bootstrap_batch -> show_sig_bootstrap_error
+sig_fit_bootstrap_batch -> show_sig_bootstrap_stability
+
+sig1 -> show_sig_profile
+sig1 -> show_sig_exposure
+sig2 -> show_sig_profile
+sig2 -> show_sig_exposure
+cosmic -> show_cosmic_sig_profile
+
 }")  -> x
+
+# Usage
+#gene [label = 'Gene of interest\ne.g. from pull-down']
+# microarray -> genelist #gseKEGG[lhead=clusterC]
+# enrichPathway -> enrichMeSH  [style = invis]
+# gseDGN -> cnetplot [ltail=clusterC]
 
 x
 
-#yyplot::gv2file(x,  file = 'clusterProfiler.pdf')
-yyplot::gv2file(x, file = 'figures/clusterProfiler-diagram.png' )
+#yyplot::gv2file(x,  file = 'sigminer-diagram.pdf')
+yyplot::gv2file(x, file = 'sigminer-diagram.png' )
